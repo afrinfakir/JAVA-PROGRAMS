61212c7cb57f85391fbf59796f3d448d62f5624c
afrinfakir <afrin.fakir33@gmail.com>
2024-09-02 15:54:26 +0530

    IsNumber method added

--- src/javaSessions/IsNumber.java
+++ src/javaSessions/IsNumber.java
@@ -4,21 +4,34 @@ public class IsNumber {
 
 	public static void main(String[] args) {
 
-		isNumber("344n");
-	}
-	
-	public static boolean isNumber(String number) {
+		isPhoneNumber("9172755864");
 		
-		try {
-		Long.parseLong(number);
-		System.out.println("Valid NUmber : "+ number);
 	}
-		catch(NumberFormatException e) {
-			System.out.println("Not a valid Number :"+ number);
+
+	public static boolean isNumber(String number) {
+
+		try {
+			Long.parseLong(number);
+			System.out.println("Valid NUmber : " + number);
+		} catch (NumberFormatException e) {
+			System.out.println("Not a valid Number :" + number);
 			return false;
 		}
-		
 		return true;
+	}
 
-}
+	public static boolean isPhoneNumber(String number) {
+
+		if (number.length() == 10 && isNumber(number)) {
+
+			System.out.println("Valid phone number : " + number);
+			return true;
+		}
+		System.out.println("Not a valid phone number : " + number);
+		return false;
+
+	}
+	
+	
+
 }
